1e2b099575c0e605384cd2e2d565814e3e1dbb5e
Denise Anjos <deniseb.anjos@outlook.com>
2021-07-25 22:14:11 -0300

    adicionando rotas na pagina inicial

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as S from './styled';
 
 export default function Home() {
@@ -6,6 +7,10 @@ export default function Home() {
     return (
         <>
         <S.TituloH1>Home works</S.TituloH1>
+        <Link to="/clientes">Clientes</Link>
+        <div>
+        <Link to="/produtos">Produtos</Link>
+        </div>
         </>
     );
 }
