13c7a4613d7a3907819ab17974a6906857cb723d
Denise Anjos <deniseb.anjos@outlook.com>
2021-07-27 18:27:06 -0300

    corrigindo href

--- src/componentes/rodape/index.js
+++ src/componentes/rodape/index.js
@@ -5,7 +5,7 @@ export default function Rodape(){
 
     return (
         <S.Rodape>
-        <p> Desenvolvido por <a href="https://github.com/deniseanjos" target="_blank">Denise Anjos</a>. </p>
+        <p> Desenvolvido por <a href="https://github.com/deniseanjos" rel="noreferrer" target="_blank">Denise Anjos</a>. </p>
         </S.Rodape>
     );
 }
